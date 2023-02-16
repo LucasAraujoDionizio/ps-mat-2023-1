a1d3668ac78c8100094274cc9b8036a21c27e9f8
Lucas Araujo da Silva Dionizio <97853963+LucasAraujoDionizio@users.noreply.github.com>
2023-02-16 14:04:14 +0000

    (16/02) criação da primeira model e migration

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//carregar as variáveis de ambiente do arquivo
+// .env para aplicacão
+require('dotenv').config()
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -8,6 +11,18 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//conexão com banco----
+const db = require('./models')
+
+try{
+    db.sequelize.authenticate()
+    console.log('SEQUELIZE : connection has been established sucessfully')
+}
+catch(error){
+    console.log('* SEQUELIZE: unable to connect to the database: ', error)
+    process.exit(1) //encerra server com erro
+}
+//------
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

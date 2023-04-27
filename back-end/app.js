ad6ee0399e8600b0498ee1f054bb173c5b1d6a0d
Lucas Araujo da Silva Dionizio <97853963+LucasAraujoDionizio@users.noreply.github.com>
2023-04-27 12:10:20 +0000

    (27/04)

--- back-end/app.js
+++ back-end/app.js
@@ -1,7 +1,7 @@
-//Carrega as variaveis de ambiente do arquivo .env para a aplicação
+// Carrega as variáveis de ambiente do arquivo
+// .env para a aplicação
 require('dotenv').config()
 
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -9,25 +9,23 @@ var logger = require('morgan');
 
 var app = express();
 
-//habilita que qualquer origem de front end possa acessar o backend
-const cors = require ('cors')
+// Habilita que qualquer origem de front-end possa
+// acessar o back-end
+const cors = require('cors')
 app.use(cors())
 
-// Conexão ao BD ---------------------------------------------------------------------
+// Conexão ao BD ------------------------------------------
 const db = require('./models')
 
 try {
   db.sequelize.authenticate()
-  console.log('SEQUELIZE: connection has been established successfully')
+  console.log('SEQUELIZE: connection has been established successfully.')
 }
-catch(error){
-  console.log('* SEQUELIZE: unable to connect to the database', error)
-  process.exit(1) //Encerra o servidor com erro  
+catch(error) {
+  console.error('* SEQUELIZE: unable to connect to the database: ', error)
+  process.exit(1)     // Encerra o servidor com erro
 }
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// ---------------------------------------------------------
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -35,49 +33,31 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//chama a verificação de autenticação para qualquer rota 
+// Chama a verificação de autenticação para qualquer rota
 const auth = require('./lib/auth')
 app.use(auth)
-/**********************************Rotas******************************** */
+
+/********************* ROTAS ***************************/
 const users = require('./routes/users')
 app.use('/users', users)
- 
-const channels = require ('./routes/channels')
+
+const channels = require('./routes/channels')
 app.use('/channels', channels)
 
-
-const paymentMethods = require ('./routes/payment_methods')
-app.use('/payment_methods', paymentMethods)
-
-const carriers = require ('./routes/carriers')
-app.use('/carriers', carriers)
-
-const shipmentPriorities = require ('./routes/shipment_priorities')
-app.use('/shipment_priorities', shipmentPriorities)
-
-const orderTags = require ('./routes/order_tags')
-app.use('/order_tags', orderTags)
-
-const tags = require ('./routes/tags')
-app.use('/tags', tags)
-
-const customerTags = require ('./routes/customer_tags')
-app.use('/customer_tags', customerTags)
-
-const customers = require ('./routes/customers')
-app.use('/customers', customers)
-
-const cities = require ('./routes/cities')
+const cities = require('./routes/cities')
 app.use('/cities', cities)
 
-const orderStatuses = require ('./routes/order_statuses')
-app.use('/order_statuses', orderStatuses)
+const customerTags = require('./routes/customer_tags')
+app.use('/customer_tags', customerTags)
 
-const orderRelStatuses = require ('./routes/order_rel_statuses')
-app.use('/order_rel_statuses', orderRelStatuses)
+const customers = require('./routes/customers')
+app.use('/customers', customers)
 
-const orders = require ('./routes/orders')
-app.use('/orders', orders)
+const paymentMethods = require('./routes/payment_methods')
+app.use('/payment_methods', paymentMethods)
+
+const tags = require('./routes/tags')
+app.use('/tags', tags)
 
 const suppliers = require('./routes/suppliers')
 app.use('/suppliers', suppliers)
--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -3,15 +3,21 @@ import Typography from '@mui/material/Typography'
 import Paper from '@mui/material/Paper'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
-import { Backdrop } from '@mui/material/Backdrop'
+import Backdrop from '@mui/material/Backdrop'
 import CircularProgress from '@mui/material/CircularProgress'
-
+import Snackbar from '@mui/material/Snackbar'
+import Alert from '@mui/material/Alert'
 
 export default function Login() {
 
   const [email, setEmail] = React.useState('')
   const [password, setPassword] = React.useState('')
   const [showWaiting, setShowWaiting] = React.useState(false)
+  const [snack, setSnack] = React.useState({
+    show: false,
+    message: '',
+    severity: 'success' // ou 'error'
+  })
 
   function handleChange(event) {
     if (event.target.name === 'email') setEmail(event.target.value)
@@ -20,36 +26,69 @@ export default function Login() {
 
   async function handleSubmit(event) {
     event.preventDefault()      // Impede o recarregamento da página
-    setShowWaiting(true) //Mostra o spinner de espera
+    setShowWaiting(true)        // Mostra o spinner de espera
     try {
       let response = await fetch('http://localhost:3000/users/login', {
         method: "POST",
         body: JSON.stringify({email, password}),
         headers: {"Content-type": "application/json; charset=UTF-8"}
       })
-      const result = await response.json()
-      console.log({result})
+      
+      console.log(response)
 
-      //Grava o token recebido do localStorage 
-      //problema sério de segurança 
+      if(response.ok) {
+        const result = await response.json()
+      
+        console.log({result})
+
+        // Grava o token recebido no localStorage
+        // ISSO É UM SÉRIO PROBLEMA DE SEGURANÇA, temos de consertar depois
+        window.localStorage.setItem('token', result.token)
+
+        // Exibe o snackbar de sucesso
+        setSnack({
+          show: true,
+          message: 'Autenticação realizada com sucesso!',
+          severity: 'success'
+        })
+      }
+      else throw new Error('Usuário ou senha incorretos')
     }
     catch(error) {
       console.error(error)
+      // Exibe o snackbar de erro
+      setSnack({
+        show: true,
+        message: error.message,
+        severity: 'error'
+      })
     }
-    finally{
-      setShowWaiting(false) //Esconde o spinner de esperaaaaaaaaaq
+    finally {
+      setShowWaiting(false)   // Esconde o spinner de espera
     }
   }
+
+  function handleSnackClose(event, reason) {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnack({ show: false })
+  };
 
   return (
     <>
       <Backdrop
         sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={open}
-        onClick={handleClose}
+        open={showWaiting}
       >
         <CircularProgress color="inherit" />
       </Backdrop>
+
+      <Snackbar open={snack.show} autoHideDuration={4000} onClose={handleSnackClose}>
+        <Alert onClose={handleSnackClose} severity={snack.severity} sx={{ width: '100%' }}>
+          {snack.message}
+        </Alert>
+      </Snackbar>
 
       <Typography variant="h3" component="h1" sx={{ textAlign: 'center' }}>
         Autentique-se

c0c12b2bf6373317693c85549c6995f4befda5ae
Lucas Araujo da Silva Dionizio <97853963+LucasAraujoDionizio@users.noreply.github.com>
2023-05-16 13:47:06 +0000

    (16/05)

--- front-end/src/models/PaymentMethod.jsx
+++ front-end/src/models/PaymentMethod.jsx
@@ -1,18 +1,20 @@
-const Joi = require ('joi')
+import Joi from 'joi'
 
 const PaymentMethod = Joi.object({
 
-    description: Joi.string()
-        .min(2)
-        .max(30)
-        .required()
-        .label('A descrição é obrigatória (entre 2 e 30 caracteres)'),
+  description: Joi.string()
+    .min(2)
+    .max(30)
+    .required()
+    .messages({'*': 'A descrição é obrigatória (entre 2 e 30 caracteres)'}),
 
-    operator_fee: Joi.number()
-        .min(0) //não aceita negativo
-        .max(100)
-        .require()
-        .label('A taxa de operação deve ser informada (entre 0 e 100')
+
+  operator_fee: Joi.number()
+    .min(0)   // Não aceita negativo
+    .max(100)
+    .required()
+    .messages({'*': 'A taxa de operação deve ser informada (entre 0 e 100)'})
+
 })
 
 export default PaymentMethod
--- front-end/src/pages/payment_method/PaymentMethodForm.jsx
+++ front-end/src/pages/payment_method/PaymentMethodForm.jsx
@@ -6,17 +6,34 @@ import Fab from '@mui/material/Fab'
 import myfetch from '../../utils/myfetch'
 import Backdrop from '@mui/material/Backdrop'
 import CircularProgress from '@mui/material/CircularProgress'
+import Notification from '../../components/ui/Notification'
+import { useNavigate } from 'react-router-dom'
+import PaymentMethod from '../../models/PaymentMethod'
+import getValidationMessages from '../../utils/getValidationMessages'
 
 export default function PaymentMethodForm() {
   const API_PATH = '/payment_methods'
 
+  const navigate = useNavigate()
+
   const [state, setState] = React.useState({
-    paymentMethod: {}, // Objeto vazio
-    showWaiting: false
+    paymentMethod: {
+      description: '',
+      operator_fee: ''
+    },
+    errors: {},
+    showWaiting: false,
+    notif: {
+      show: false,
+      severity: 'success',
+      message: ''
+    }
   })
   const {
     paymentMethod,
-    showWaiting
+    errors,
+    showWaiting,
+    notif
   } = state
 
   function handleFormFieldChange(event) {
@@ -27,24 +44,55 @@ export default function PaymentMethodForm() {
 
   function handleFormSubmit(event) {
     event.preventDefault()    // Evita que a página seja recarregada
-
     // Envia os dados para o back-end
     sendData()
   }
 
   async function sendData() {
-    setState({...state, showWaiting: true})
+    setState({...state, showWaiting: true, errors: {}})
     try {
+      
+      // Chama a validação da biblioteca Joi
+      await PaymentMethod.validateAsync(paymentMethod, { abortEarly: false })
+
       await myfetch.post(API_PATH, paymentMethod)
-      // DAR FEEDBACK POSITIVO E VOLTAR PARA A LISTAGEM
-      alert('Salvou!')
-      setState({...state, showWaiting: false})
+      setState({
+        ...state, 
+        showWaiting: false,
+        notif: {
+          severity: 'success',
+          show: true,
+          message: 'Novo item salvo com sucesso'
+        }
+      })
     }
     catch(error) {
+      const { validationError, errorMessages } = getValidationMessages(error)
+
       console.error(error)
-      // DAR FEEDBACK NEGATIVO
-      setState({...state, showWaiting: false})
+      
+      setState({
+        ...state, 
+        showWaiting: false,
+        errors: errorMessages,
+        notif: {
+          severity: 'error',
+          show: !validationError,
+          message: 'ERRO: ' + error.message
+        }
+      })
     }
+  }
+
+  function handleNotifClose(event, reason) {
+    if (reason === 'clickaway') {
+      return;
+    }
+    
+    // Se o item foi salvo com sucesso, retorna à página de listagem
+    if(notif.severity === 'success') navigate(-1)
+
+    setState({ ...state, notif: { ...notif, show: false } })
   }
 
   return (
@@ -55,8 +103,18 @@ export default function PaymentMethodForm() {
       >
         <CircularProgress color="inherit" />
       </Backdrop>
+
+      <Notification 
+        show={notif.show}
+        severity={notif.severity}
+        onClose={handleNotifClose}
+      >
+        {notif.message}
+      </Notification>
       
       <PageTitle title="Cadastrar novo método de pagamento" />
+
+      <div>{notif.severity}</div>
 
       <form onSubmit={handleFormSubmit}>
         <TextField 
@@ -67,6 +125,8 @@ export default function PaymentMethodForm() {
           name="description"  // Nome do campo na tabela
           value={paymentMethod.description}   // Nome do campo na tabela
           onChange={handleFormFieldChange}
+          error={errors?.description}
+          helperText={errors?.description}
         />
 
         <TextField 
@@ -78,6 +138,8 @@ export default function PaymentMethodForm() {
           name="operator_fee"  // Nome do campo na tabela
           value={paymentMethod.operator_fee}   // Nome do campo na tabela
           onChange={handleFormFieldChange}
+          error={errors?.operator_fee}
+          helperText={errors?.operator_fee}
         />
 
         <Fab 
